be4b529c6436ff467f4a85003486c3f14717d05a
omar alibi <alibiomar3@gmail.com>
2024-04-15 15:58:40 +0100

    omar

---
+++ crud.js
@@ -0,0 +1,62 @@
+
+const tableBody = document.getElementById("table-body");
+
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+getUsers();
+
+  function updateUser() {
+    users[currentUserIndex].name = nameInput.value;
+    users[currentUserIndex].phone = phoneInput.value;
+    users[currentUserIndex].number = numberInput.value;
+    users[currentUserIndex].date = dateInput.value;
+    saveData();
+  }
+
+  function showUpdateUser(index) {
+    currentUserIndex = index;
+    nameInput.value = users[index].name;
+    phoneInput.value = users[index].phone;
+    numberInput.value = users[index].number;
+    dateInput.value = users[index].date;
+}
+function deleteUser(index) {
+  users.splice(index, 1);
+  saveData();
+  getUsers();
+}
+
+  
+function getUsers() {
+  tableBody.innerHTML = "";
+  users.forEach((user, index) => {
+      const tr = document.createElement("tr");
+      const nameTd = document.createElement("td");
+      const phoneTd = document.createElement("td");
+      const numberTd = document.createElement("td");
+      const dateTd = document.createElement("td");
+      const actionTd = document.createElement("td"); // Create actionTd for action buttons
+
+      const deleteBtn = document.createElement("button");
+      deleteBtn.className = "delete-btn";
+      deleteBtn.innerText = "Delete";
+      deleteBtn.addEventListener("click", () => deleteUser(index));
+
+      dateTd.innerText = user.date;
+      nameTd.innerText = user.name;
+      phoneTd.innerText = user.phone;
+      numberTd.innerText = user.number; // Added to display the number
+
+      actionTd.appendChild(editBtn);
+      actionTd.appendChild(deleteBtn);
+
+      tr.appendChild(phoneTd);
+      tr.appendChild(nameTd);
+      tr.appendChild(numberTd); // Append numberTd
+      tr.appendChild(dateTd);
+      tr.appendChild(actionTd); // Append actionTd
+      
+      tableBody.appendChild(tr);
+  });
+}
+
