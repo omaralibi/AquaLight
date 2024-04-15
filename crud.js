bcbcf9c396c1d8baa51e1eb618ed886921cc6182
omar alibi <alibiomar3@gmail.com>
2024-04-15 16:52:00 +0100

    omar

--- crud.js
+++ crud.js
@@ -3,59 +3,55 @@ const tableBody = document.getElementById("table-body");
 
 let users = JSON.parse(localStorage.getItem("users")) || [];
 
-getUsers();
+function displayUser(user) {
+  const tr = document.createElement("tr");
+  const nameTd = document.createElement("td");
+  const phoneTd = document.createElement("td");
+  const numberTd = document.createElement("td");
+  const dateTd = document.createElement("td");
+  const actionTd = document.createElement("td");
 
-  function updateUser() {
-    users[currentUserIndex].name = nameInput.value;
-    users[currentUserIndex].phone = phoneInput.value;
-    users[currentUserIndex].number = numberInput.value;
-    users[currentUserIndex].date = dateInput.value;
-    saveData();
-  }
+  nameTd.innerText = user.name;
+  phoneTd.innerText = user.phone;
+  numberTd.innerText = user.number;
+  dateTd.innerText = user.date;
 
-  function showUpdateUser(index) {
-    currentUserIndex = index;
-    nameInput.value = users[index].name;
-    phoneInput.value = users[index].phone;
-    numberInput.value = users[index].number;
-    dateInput.value = users[index].date;
+  const deleteBtn = document.createElement("button");
+  deleteBtn.className = "delete-btn";
+  deleteBtn.innerText = "Delete";
+  deleteBtn.setAttribute("data-name", user.name); 
+  deleteBtn.addEventListener("click", deleteUser); 
+
+
+  actionTd.appendChild(deleteBtn);
+
+  tr.appendChild(nameTd);
+  tr.appendChild(phoneTd);
+  tr.appendChild(numberTd);
+  tr.appendChild(dateTd);
+  tr.appendChild(actionTd);
+
+  tableBody.appendChild(tr);
 }
-function deleteUser(index) {
-  users.splice(index, 1);
-  saveData();
-  getUsers();
-}
 
+function deleteUser(event) {
+  const userName = event.target.getAttribute("data-name"); 
   
+  users = users.filter(user => user.name !== userName); 
+  saveData();
+  tableBody.innerHTML = ""; 
+  getUsers(); 
+}
+function saveData() {
+  localStorage.setItem("users", JSON.stringify(users));
+}
+
 function getUsers() {
   tableBody.innerHTML = "";
-  users.forEach((user, index) => {
-      const tr = document.createElement("tr");
-      const nameTd = document.createElement("td");
-      const phoneTd = document.createElement("td");
-      const numberTd = document.createElement("td");
-      const dateTd = document.createElement("td");
-      const actionTd = document.createElement("td"); 
-
-      const deleteBtn = document.createElement("button");
-      deleteBtn.className = "delete-btn";
-      deleteBtn.innerText = "Delete";
-      deleteBtn.addEventListener("click", () => deleteUser(index));
-
-      dateTd.innerText = user.date;
-      nameTd.innerText = user.name;
-      phoneTd.innerText = user.phone;
-      numberTd.innerText = user.number;
-      actionTd.appendChild(editBtn);
-      actionTd.appendChild(deleteBtn);
-
-      tr.appendChild(phoneTd);
-      tr.appendChild(nameTd);
-      tr.appendChild(numberTd); 
-      tr.appendChild(dateTd);
-      tr.appendChild(actionTd);
-      
-      tableBody.appendChild(tr);
+  users.forEach((user) => {
+      displayUser(user); 
   });
 }
+
+getUsers();
 
